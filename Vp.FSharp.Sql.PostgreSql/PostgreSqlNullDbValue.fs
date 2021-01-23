587b70469f2d0bad4cedc832c5ce4919615772bb
Kerry Perret <kerry.perret@outlook.com>
2021-01-23 17:08:09 +0100

    feat(refactor): add specific support

---
+++ Vp.FSharp.Sql.PostgreSql/PostgreSqlNullDbValue.fs
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.PostgreSql.PostgreSqlNullDbValue
+
+open Vp.FSharp.Sql
+
+
+let ifNone toDbValue = NullDbValue.ifNone toDbValue PostgreSqlDbValue.Null
+let ifError toDbValue = NullDbValue.ifError toDbValue (fun _ -> PostgreSqlDbValue.Null)

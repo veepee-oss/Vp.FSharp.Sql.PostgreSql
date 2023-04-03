a06661114973d0bb2985b6068c8393b3320b45d3
Natalie Perret <mperret@veepee.com>
2023-04-03 13:49:14 +0200

    feat: add generic facilities and update to dotnet 6

--- Vp.FSharp.Sql.PostgreSql.Tests/Tests.fs
+++ Vp.FSharp.Sql.PostgreSql.Tests/Tests.fs
@@ -1,8 +1,12 @@
 module Tests
 
+open NpgsqlTypes
+open Vp.FSharp.Sql.PostgreSql
 open Xunit
 
 
 [<Fact>]
 let ``My test`` () =
+    let dbValue1 = PostgreSqlDbValue.Array<int> (NpgsqlDbType.Integer, [| 324 |])
+    let dbValue2 = PostgreSqlDbValue.Array<int> (NpgsqlDbType.Integer, [| "sdf" |])
     Assert.True(true)
